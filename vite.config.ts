cfd6f8542be2781efb9f18eaf1d2305a6c1ad306
Fredrik Stahre <fredrik@starhem.com>
2026-06-05 08:59:44 +0200

    compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   // Use relative asset paths so dist/index.html works when opened via file://
   base: "./",
+  esbuild: {
+    target: "es2019",
+  },
   plugins: [
+    legacy({
+      targets: ["safari >= 12"],
+      modernPolyfills: true,
+    }),
     {
       name: "remove-crossorigin",
       transformIndexHtml(html) {
